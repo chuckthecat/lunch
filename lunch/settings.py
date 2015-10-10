51f52eb499b854856eb2a217b4af81e168fee73c
aergener <aergener@gmail.com>
2015-10-10 17:02:43 -0400

    Fix Vagrantfile

--- lunch/settings.py
+++ lunch/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'lunch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lunch',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',  # Set to empty string for localhost.
+        'PORT': '',  # Set to empty string for default.
+        'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
     }
 }
 
